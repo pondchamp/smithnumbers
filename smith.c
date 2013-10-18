0655b2705716847332833f49d08e35a46a9ffe9a
stablefree <jubian@me.com>
2013-10-19 01:00:53 +1100

    switched to ull data type; arrays now malloced

--- smith.c
+++ smith.c
@@ -10,43 +10,46 @@
 #include <assert.h>
 #include <ctype.h>
 
-#define MAX_FACTOR (int) (max-1)/2
+#define MAX_FACTOR (unsigned long long) (max-1)/2
 #define ASCII_TO_NUM 48
 #define MAX_COL 10
 
-void primeFactors(int max, int *factors);
-int addFactors(int max, int *factors);
-int isSmith (int max);
+void primeFactors(unsigned long long max, unsigned long long *factors);
+unsigned long long addFactors(unsigned long long max, unsigned long long *factors);
+int isSmith (unsigned long long max);
 
 int main (int argc, char *argv[]) {
     assert(argc == 3);
-    int max = atoi(argv[2]), count;
+    unsigned long long max = atoi(argv[2]), count;
     assert (max > 0);
     int col = 0;
     if (!strcmp(argv[1], "-all")) {
         for (count = 2; count <= max; count++) {
-            if (isSmith (count)) {printf("%d\t", count); col++;}
+            if (isSmith (count)) {printf("%llu\t", count); col++;}
             if (col == MAX_COL) {printf("\n"); col = 0;}
         }
     } else if (!strcmp(argv[1], "-check")) {
-        if (isSmith (max)) printf("%d is a Smith number!\n", max);
-        else printf("%d is not a Smith number!\n", max);
+        if (isSmith (max)) printf("%llu is a Smith number!\n", max);
+        else printf("%llu is not a Smith number!\n", max);
     } else printf("Invalid arguments. Valid arguments are '-all' and '-check'.\n");
     return EXIT_SUCCESS;
 }
 
-int isSmith (int max) {
-    int i, factors[MAX_FACTOR], sumDigits = 0, sumFactors = 0;
+int isSmith (unsigned long long max) {
+    int i, sumDigits = 0, sumFactors = 0;
+    unsigned long long *factors = malloc(sizeof(unsigned long long)*MAX_FACTOR);
+    assert(factors != NULL);
     char digits[50];
     primeFactors(max, factors);
-    sprintf(digits, "%d", max);
+    sprintf(digits, "%llu", max);
     for (i = 0; digits[i] != '\0'; i++) sumDigits += (int) digits[i] - ASCII_TO_NUM;
     sumFactors = addFactors(max, factors);
+    free(factors);
     return (sumFactors == sumDigits);
 }
 
-void primeFactors(int max, int *factors) {
-    int remain = max;
+void primeFactors(unsigned long long max, unsigned long long *factors) {
+    unsigned long long remain = max;
     int i, j = 0;
     for (i = 0; i < MAX_FACTOR; i++) factors[i] = -1;
     while (remain != 1) {
@@ -64,8 +67,9 @@ void primeFactors(int max, int *factors) {
     }
 }
 
-int addFactors(int max, int *factors) {
-    int i = 0, sum = 0, n;
+unsigned long long addFactors(unsigned long long max, unsigned long long *factors) {
+    int i = 0, n;
+    unsigned long long sum = 0;
     while ((i < MAX_FACTOR) && (factors[i] != -1)) {
         n = factors[i];
         while (n > 0) {
